77d955ac7e08665232a7bcfcba7f615309724908
patorma <pocontreras@ing.ucsc.cl>
2020-06-24 03:17:53 -0400

    se vio lo de las subconsultas

--- agregacion.sql
+++ agregacion.sql
@@ -1,3 +1,29 @@
  SELECT CONCAT(nombre," ",apellido) AS nombre_completo, "", pais_origen AS email FROM autores
  UNION
   SELECT CONCAT(nombre," ",apellidos) AS nombre_completo, email , "" FROM usuarios;
+
+-- sin subconsultas
+-- SELECT AVG(ventas) FROM libros;
+-- --  273.4909
+-- SET @promedio = (SELECT AVG(ventas) FROM libros);
+
+-- SELECT 
+-- autor_id 
+-- FROM libros
+--  GROUP BY autor_id 
+--  HAVING SUM(ventas) >  @promedio;
+
+SET @promedio = (SELECT AVG(ventas) FROM libros);
+
+-- El orden en la que se ejecutan las subconsultas son del nivel mas bajo al mas alto
+
+-- como es una lista se agrega IN 
+
+SELECT CONCAT(nombre, " ", apellido) AS nombre
+FROM autores
+WHERE autor_id IN(
+SELECT 
+autor_id 
+FROM libros
+ GROUP BY autor_id 
+ HAVING SUM(ventas) > (SELECT AVG(ventas) FROM libros));
