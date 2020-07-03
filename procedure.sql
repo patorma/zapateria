4dc54cc9ddef4102054608c9ae2c03d010d4d9c3
patorma <pocontreras@ing.ucsc.cl>
2020-07-03 19:32:38 -0400

    se vio la introduccion de procedimiento y como eliminarlos

---
+++ procedure.sql
@@ -0,0 +1,16 @@
+-- generar procedimiento almacenado que permita prestar libros
+DELIMITER  //
+
+CREATE PROCEDURE prestamo(usuario_id INT, libro_id INT)
+BEGIN
+    INSERT INTO libros_usuarios(libro_id,usuario_id) VALUES (libro_id, usuario_id);
+    UPDATE libros SET stock = stock - 1 WHERE libros.libro_id = libro_id;
+    
+END//
+
+
+DELIMITER ;
+
+
+
+-- eliminar un procedimiento:
