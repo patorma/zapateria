04e89cc1fe34fef6a16fcdd0c2e1188d5bf2d243
patorma <pocontreras@ing.ucsc.cl>
2021-01-20 01:42:54 -0300

    se modifico modelo de gastos

--- gastos.sql
+++ gastos.sql
@@ -24,31 +24,33 @@ CREATE TABLE IF NOT EXISTS usuarios(
 
 )ENGINE=INNODB;
 
-
-CREATE TABLE IF NOT EXISTS tipos(
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(50) NOT NULL,
-    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
-)ENGINE=INNODB;
-
+-- CREATE TABLE IF NOT EXISTS tipos(
+--     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+--     tipo VARCHAR(50) NOT NULL,
+--     fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
+-- )ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS gastos(
      id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
      nombre VARCHAR(60) NOT NULL,
-     fecha DATE NOT NULL,
-     tipo_id INT UNSIGNED NOT NULL,
+     valor INT UNSIGNED NOT NULL,
+     tipo ENUM ('GASTO_COMUN','OTROS') NOT NULL,
+     fecha_gasto DATE NOT NULL,
      usuario_id INT UNSIGNED NOT NULL,
-     FOREIGN KEY (tipo_id) REFERENCES tipos(id) ON DELETE CASCADE,
-     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE 
+     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
 )ENGINE=INNODB;
+
+
 
 CREATE TABLE IF NOT EXISTS detalle_gastos(
      id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
      descripcion VARCHAR(400) NOT NULL,
      cantidad INT UNSIGNED NOT NULL,
-     valor INT UNSIGNED NOT NULL,
+     nombre_local VARCHAR(60) NOT NULL,
+     ciudad  VARCHAR(45) NOT NULL,
      gasto_id INT UNSIGNED NOT NULL,
      FOREIGN KEY (gasto_id) REFERENCES gastos(id) ON DELETE CASCADE 
+
 )ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS notas(
@@ -71,5 +73,36 @@ INSERT INTO usuarios(nombre, apellidos,email, contrasena,estado,rol_id)
 -- TODOS LOS USUARIOS SEAN INVITADOS
 ALTER TABLE usuarios ALTER COLUMN rol_id SET DEFAULT 2;
 
+INSERT INTO usuarios(nombre, apellidos,email, contrasena,estado) 
+        VALUES('Carlos', 'Suarez', 'gato@gmail.com','poto','activo');
+-- INSERT INTO gastos (nombre,valor,fecha_gasto,usuario_id ) VALUES()
 
+-- Crea los tipos de gastos
+-- INSERT INTO tipos (tipo) VALUES ('gasto común');
+-- INSERT INTO tipos (tipo) VALUES ('supermercado');
+-- INSERT INTO tipos (tipo) VALUES ('ferreteria');
+-- INSERT INTO tipos (tipo) VALUES ('vega');
 
+-- Crea gastos de ejemplos
+INSERT INTO gastos (nombre,valor,tipo,fecha_gasto,usuario_id) 
+VALUES ('Luz',17000,'GASTO_COMUN','2021-01-13',2);
+
+INSERT INTO gastos (nombre,valor,tipo,fecha_gasto,usuario_id) 
+VALUES ('Agua',7000,'GASTO_COMUN','2021-01-10',2);
+
+INSERT INTO gastos (nombre,valor,tipo,fecha_gasto,usuario_id) 
+VALUES ('Supermercado',25000,'OTROS','2021-01-25',2);
+
+INSERT INTO detalle_gastos (descripcion,cantidad,nombre_local,ciudad,gasto_id) 
+VALUES ('Se fue a pagar la boleta de luz de frontel',1,'sucursal frontel','Laja',1);
+
+INSERT INTO detalle_gastos (descripcion,cantidad,nombre_local,ciudad,gasto_id) 
+VALUES ('Se fue a pagar la boleta del agua',1,'supermercado unico','Laja',2);
+
+INSERT INTO detalle_gastos (descripcion,cantidad,nombre_local,ciudad,gasto_id) 
+VALUES ('Se compro carne, botella de limon, sarten y bolsas de yogurt',4,'supermercado unimarc','Laja',3);
+
+SELECT (g.tipo) AS tipo,(g.nombre) AS gasto,(g.valor) AS valor,(d.descripcion) AS
+ descripcion,
+(d.cantidad) AS cantidad,(d.nombre_local) AS local,(d.ciudad) AS ciudad, (g.fecha_gasto) AS fecha
+ FROM  gastos AS g INNER JOIN  detalle_gastos AS d ON(g.id = d.gasto_id);
